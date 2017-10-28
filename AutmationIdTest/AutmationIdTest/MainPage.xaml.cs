83da0ea413f4534cccd3ae946399ea5b202de27e
Nathan Hüsken <nathan@wintercloud.de>
2017-10-28 19:04:55 +0200

    Test project with up do date xamarin forms

---
+++ AutmationIdTest/AutmationIdTest/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AutmationIdTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<String> ItemsSource { get; set; } = new ObservableCollection<string> { "1", "2", "3" };
+
+        public MainPage()
+        {
+            InitializeComponent();
+            this.BindingContext = this;
+        }
+    }
+}
